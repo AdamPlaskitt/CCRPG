df5455316162b283aa87203d3b3f0e3bc5225cfe
Adam Plaskitt <adam.plaskitt1@gmail.com>
2020-11-14 19:02:11 +0000

    Fix PEP8 whitespace issues in top of file

--- ccrpg/Pygame_screen_load.py
+++ ccrpg/Pygame_screen_load.py
@@ -3,24 +3,24 @@ import time
 import sys
 
 FPS = 15
-WINDOWWIDTH = 640
-WINDOWHEIGHT = 480
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
 CELLSIZE = 20
-assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
-assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
-CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
-CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
-screen=pygame.display.set_mode((0,0))
+assert WINDOW_WIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOW_HEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+CELLWIDTH = int(WINDOW_WIDTH / CELLSIZE)
+CELLHEIGHT = int(WINDOW_HEIGHT / CELLSIZE)
+screen = pygame.display.set_mode((0, 0))
 
 
-#             R    G    B
-WHITE     = (255, 255, 255)
-BLACK     = (  0,   0,   0)
-RED       = (255,   0,   0)
-GREEN     = (  0, 255,   0)
-DARKGREEN = (  0, 155,   0)
-DARKGRAY  = ( 40,  40,  40)
-BGCOLOR = BLACK
+#           R    G    B
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+DARKGREEN = (0, 155, 0)
+DARKGRAY = (40, 40, 40)
+BACKGROUND_COLOR = BLACK
 
 UP = 'up'
 DOWN = 'down'
@@ -29,7 +29,7 @@ RIGHT = 'right'
 
 pygame.init()
 FPSCLOCK = pygame.time.Clock()
-DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 pygame.display.set_caption('test')
 
