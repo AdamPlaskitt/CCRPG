db7f76c9d87302dabf191050f84fc97438cb4145
Adam Plaskitt <adam.plaskitt1@gmail.com>
2020-11-14 19:03:35 +0000

    Fix PEP8 nameing convention for consts in file

--- ccrpg/Pygame_screen_load.py
+++ ccrpg/Pygame_screen_load.py
@@ -5,11 +5,11 @@ import sys
 FPS = 15
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
-CELLSIZE = 20
-assert WINDOW_WIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
-assert WINDOW_HEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
-CELLWIDTH = int(WINDOW_WIDTH / CELLSIZE)
-CELLHEIGHT = int(WINDOW_HEIGHT / CELLSIZE)
+CELL_SIZE = 20
+assert WINDOW_WIDTH % CELL_SIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOW_HEIGHT % CELL_SIZE == 0, "Window height must be a multiple of cell size."
+CELL_WIDTH = int(WINDOW_WIDTH / CELL_SIZE)
+CELL_HEIGHT = int(WINDOW_HEIGHT / CELL_SIZE)
 screen = pygame.display.set_mode((0, 0))
 
 
@@ -28,9 +28,9 @@ LEFT = 'left'
 RIGHT = 'right'
 
 pygame.init()
-FPSCLOCK = pygame.time.Clock()
-DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+FPS_CLOCK = pygame.time.Clock()
+DISPLAY_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+BASIC_FONT = pygame.font.Font('freesansbold.ttf', 18)
 pygame.display.set_caption('test')
 
 screen.fill(WHITE)
